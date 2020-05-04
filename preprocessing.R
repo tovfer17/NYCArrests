da6b273ae08e3390424e3a01b08c9cfbac9177a3
nbala02 <63129564+nbala02@users.noreply.github.com>
2020-05-04 16:42:16 -0400

    Update preprocessing.R

--- preprocessing.R
+++ preprocessing.R
@@ -1,8 +1,9 @@
-#Set working directory
+#---------------------------------------- Set working directory ----------------------------------------#
 #setwd('/Users/fer/Documents/RStudioFiles')
 #setwd('/Users/arpit/Documents/Data Mining Project')
 setwd('/Users/nehabala/Desktop/Data Mining Project')
 
+#------------------------------- Read Original Dataset & Clean the Data --------------------------------#
 #Read and call arrest data
 arrestData <- read.csv("NYPD_Arrest_Data__Year_to_Date_.csv",header=TRUE)
 View(arrestData)
@@ -20,31 +21,29 @@ View(arrestColumns)
 
 #Convert Arrest_Date column to date formate using as.Date function
 arrestColumns $ARREST_DATE <-as.Date(arrestColumns $ARREST_DATE,"%m/%d/%Y")
-
 #Create new column called month and changes the format to just have the month using format funciton
 arrestColumns$Month <- format(arrestColumns$ARREST_DATE,format = "%m")
-
 #Shows the arrestColumn (With the changes above)
 View(arrestColumns)
-
-#Converting month values from characters to numeric
-arrestColumns$Month <- (as.character(arrestColumns$Month))
 
 #Create a new csv file with the clean data
 newDataSet <- arrestColumns[c(2,3,4,5,6,7)]
 write.csv(newDataSet, paste("preprocessed.csv"), row.names = F)
 View(newDataSet)
 
-#Create a variable to duplicate the data
-dataset1 <- newDataSet
-str(dataset1)
+#------------------------------- Preprocessing by Converting Datatypes --------------------------------#
+#Create a variable to duplicate the data for preprocessing
+preprocessed <- newDataSet
+#Output the duplicate data with the original datatypes
+str(preprocessed)
 
-#Preprocess from Char to Factor
-dataset1$PD_DESC <- as.factor(dataset1$PD_DESC)
-dataset1$ARREST_BORO <- as.factor(dataset1$ARREST_BORO)
-dataset1$AGE_GROUP <- as.factor(dataset1$AGE_GROUP)
-dataset1$PERP_SEX <- as.factor(dataset1$PERP_SEX)
-dataset1$PERP_RACE <- as.factor(dataset1$PERP_RACE)
-dataset1$Month <- as.factor(dataset1$Month)
+#Preprocess by converting the original datatypes to Factor
+preprocessed$PD_DESC <- as.factor(preprocessed$PD_DESC)
+preprocessed$ARREST_BORO <- as.factor(preprocessed$ARREST_BORO)
+preprocessed$AGE_GROUP <- as.factor(preprocessed$AGE_GROUP)
+preprocessed$PERP_SEX <- as.factor(preprocessed$PERP_SEX)
+preprocessed$PERP_RACE <- as.factor(preprocessed$PERP_RACE)
+preprocessed$Month <- as.factor(preprocessed$Month)
 
-str(dataset1)
+#Output the duplicate data with the changed datatypes
+str(preprocessed)
