c049772e0e54b6312f84404e00dc934cf2d9ec8f
Fernanda Tovar <tovfer17@gmail.com>
2020-05-07 20:18:51 -0400

    dcjksd

--- visualization.R
+++ visualization.R
@@ -1,3 +1,38 @@
+#---------------------------------------- Set working directory ----------------------------------------#
+setwd('/Users/fer/Documents/RStudioFiles/NYCArrests')
+#setwd('/Users/arpit/Documents/Data Mining Project')
+#setwd('/Users/nehabala/Desktop/Data Mining Project')
+#--------------------------------- Install Packages & Load Libraries ----------------------------------#
+#Install the packages needed for association rule mining
+install.packages("arulesViz")
+install.packages("arules")
+#Load the required packages for association rule mining
+library(arulesViz)
+library(arules)
+
+#---------------------------------Graphs for Association Rules ---------------------------------#
+# Filter rules with confidence greater than 0.4 or 40%
+subRules<-association.rules[quality(association.rules)$confidence>0.4]
+#Plot SubRules
+plot(subRules)
+
+
+#Plotting the apriori algorithm results
+plot(ruleSet1)
+plot(ruleSet1,method="Graph")
+plot(ruleSet1, method = "two-key plot")
+head(quality(ruleSet1))
+
+#Matrix Visuals        
+subrules <- removeRedundant[quality(removeRedundant)$confidence > 0.4]
+subrules
+plot(subrules, method = "matrix", measure = "confidence", interactive = TRUE)
+
+#Grouped Visuals
+subrules2 <- head(ruleSet1, n = 235, by = "confidence")
+subrules2
+plot(subrules2, method = "grouped", interactive = TRUE)
+
 #---------------------------------- Bar Graph for Association Rules -----------------------------------#
 #Bar Grap Visuals
 if (!require("RColorBrewer")) {
@@ -8,24 +43,8 @@ if (!require("RColorBrewer")) {
 }
 
 #Output the graph for each analysis
-itemFrequencyPlot(items(analysis1),topN=29,type="absolute",col=brewer.pal(8,'Pastel2'), main="Absolute Item Frequency Plot")
-itemFrequencyPlot(items(analysis2),topN=29,type="absolute",col=brewer.pal(8,'Pastel2'), main="Absolute Item Frequency Plot")
-itemFrequencyPlot(items(analysis3),topN=29,type="absolute",col=brewer.pal(8,'Pastel2'), main="Absolute Item Frequency Plot")
+itemFrequencyPlot(items(ruleSet1),topN=29,type="absolute",col=brewer.pal(8,'Pastel2'), main="Absolute Item Frequency Plot")
+itemFrequencyPlot(items(ruleSet2),topN=29,type="absolute",col=brewer.pal(8,'Pastel2'), main="Absolute Item Frequency Plot")
+itemFrequencyPlot(items(ruleSet3),topN=29,type="absolute",col=brewer.pal(8,'Pastel2'), main="Absolute Item Frequency Plot")
 
 
-#--------------------------------- Other Graphs for Association Rules ---------------------------------#
-#Plotting the apriori algorithm results
-plot(analysis1)
-plot(analysis1,method="Graph")
-plot(analysis1, method = "two-key plot")
-head(quality(analysis1))
-
-#Matrix Visuals        
-subrules <- removeRedundant[quality(removeRedundant)$confidence > 0.4]
-subrules
-plot(subrules, method = "matrix", measure = "confidence", interactive = TRUE)
-
-#Grouped Visuals
-subrules2 <- head(analysis1, n = 235, by = "confidence")
-subrules2
-plot(subrules2, method = "grouped", interactive = TRUE)
